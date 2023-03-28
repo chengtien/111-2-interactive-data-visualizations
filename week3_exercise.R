62ddea46cbca16da087d4338163275d796c1692d
chengtien <bunny031642@gmail.com>
2023-03-28 15:44:45 +0800

    .

--- week3_exercise.R
+++ week3_exercise.R
@@ -3,6 +3,12 @@ library(dplyr)
 library(ggplot2)
 big_mac_raw_index <- read_csv("https://raw.githubusercontent.com/TheEconomist/big-mac-data/master/output-data/big-mac-raw-index.csv")
 
+big_mac_raw_index|>
+  dplyr::filter(USD>=0)|>
+  mutate(valuation="overvalued")->overvalued
+big_mac_raw_index|>
+  dplyr::filter(USD<0)|>
+  mutate(valuation="undervalued")->undervalued
 
 filter(big_mac_raw_index,USD>=0)|>mutate(valuation="overvalued")->overvalued
 filter(big_mac_raw_index,USD<0)|>mutate(valuation="undervalued")->undervalued
@@ -14,16 +20,18 @@ ggplot()+
   geom_point(data=big_mac_index,
              mapping = aes(x=date,y=USD,colour=valuation),
              alpha=0.3)+
-  scale_colour_manual(values = c("#86C8BC","#3A98B9","#CD0404"))+
+  scale_colour_manual(values = c("#86C8BC","#CD0404"))+
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         panel.grid.major.y=element_line(color="grey"),
         panel.background = element_rect(fill="white"),
         legend.position="none",
         axis.title.x = element_blank(),
-        axis.title.y = element_blank())+
-  geom_line(data=Taiwan,
-            mapping = aes(x=date,y=USD,color=name),
-            size=1)->geom
+        axis.title.y = element_blank())->geom
 geom
-plotly::ggplotly(geom)
+plotly::ggplotly(geom)->plotlyGeom
+
+
+geom_line(data=Taiwan,
+          mapping = aes(x=date,y=USD,color=name),
+          size=1)
--- week4_exercise.R
+++ week4_exercise.R
@@ -1,5 +1,23 @@
 big_mac_raw_index <- read_csv("https://raw.githubusercontent.com/TheEconomist/big-mac-data/master/output-data/big-mac-raw-index.csv")
 
+big_mac_raw_index$date|>range()->dateRange
+ddfull<-data.frame(
+  data=seq(dataRange[[1]],dataRange[[2]],by)
+)
+
+
+#資料處理
+dd3$name |> unique() -> validCountries
+dd3$date |> unique() -> validDates
+big_mac_raw_index$name|>unique()->country
+big_mac_raw_index$date|>unique()->dates
+full <- expand.grid(country,dates)
+names(full) <- c("name", "date")
+full|>
+  dplyr::left_join(big_mac_raw_index,
+                   by=c("name","date"))->BigMacIndex
+
+
 
 #用ggplot轉為plotly
 ggplot(data=big_mac_raw_index,mapping = aes(x=date,y=USD,colour=name))+
@@ -16,6 +34,7 @@ gplty1
 big_mac_raw_index|>
   plotly::highlight_key(~name)->highlighted_bigMac
 
+
 plot_ly(
   data=highlighted_bigMac,
   x=~date,
@@ -24,7 +43,39 @@ plot_ly(
   color=~name,
   alpha=0.3
   )|>
-  plotly::layout(yaxis=list(range=c(-0.8,1.8)))|>
+  plotly::layout(yaxis=list(range=c(-0.8,1.8),autorange=FALSE))|>
+  add_trace(
+    orientation=trace1$orientation,
+    width=trace1$width,
+    type=trace1$type,
+    mode="lines",
+    textposition=trace1$textposition,
+    marker=trace1$marker,
+    showlegend=trace1$showlegend,
+    xaxis=trace1$xaxis,
+    yaxis=trace1$yaxis,
+    hoverinfo=trace1$hoverinfo,
+    name=trace1$name
+  )|>
+  highlight(
+    on ="plotly_click",
+    off = "plotly_doubleclick")|>
+  hide_legend()
+############更正
+
+BigMacIndex|>
+  plotly::highlight_key(~name)->highlighted_BigMac
+
+
+plot_ly(
+  data=highlighted_BigMac,
+  x=~date,
+  y=~USD,
+  text=~name,
+  color=~name,
+  alpha=0.3
+)|>
+  plotly::layout(yaxis=list(range=c(-0.8,1.8),autorange=FALSE))|>
   add_trace(
     orientation=trace1$orientation,
     width=trace1$width,
@@ -48,7 +99,10 @@ plot_ly(
 
 
 
-
+############
+pt=econIDV::PlotlyTools()
+pt$query_trace$Scatter()->qq
+qq("line")
 
 
 big_mac_raw_index|>
